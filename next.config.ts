ac85a29dd099ba1cc5eb1a2602e64b13e94e7295
Abhi13jain <abhiijain13@gmail.com>
2025-09-18 03:22:24 +0530

    setup app,file and folder structure,routing and auth pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbopack: {
+      root: './', // or relative path to your project root
+    },
+  },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
