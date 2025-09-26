1a635ead4e4ea9740c1a1f952a9746fe374cf8f3
Abhi13jain <abhiijain13@gmail.com>
2025-09-26 21:28:19 +0530

    implement auth and storage

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -14,6 +14,9 @@ import {
 import { CustomFormField } from "./FormField"
 import { Input } from "@/components/ui/input"
 import { useRouter } from "next/navigation"
+import { auth } from "@/firebase/client"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+import { signIn, signUp } from "@/lib/actions/auth.action"
 
 // const formSchema = z.object({
 //     username: z.string().min(2).max(50),
@@ -40,13 +43,36 @@ const AuthForm = ({ type }: { type: FormType }) => {
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    async function onSubmit(values: z.infer<typeof formSchema>) {
         try {
             if (type === 'sign-up') {
+                const { name, email, password } = values;
+
+                const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+                const result = await signUp({
+                    uid: userCredentials.user.uid,
+                    name: name!,
+                    email,
+                    password,
+                })
+                if (!result?.success) {
+                    toast.error(result?.message);
+                    return;
+                }
                 toast.success('Account created successfully.Please sign in.');
                 router.push('/sign-in');
             }
             else {
+                const { email, password } = values;
+                const userCredential = await signInWithEmailAndPassword(auth, email, password);
+                const idToken = await userCredential.user.getIdToken();
+                if (!idToken) {
+                    toast.error('Sign in failed');
+                    return;
+                }
+                await signIn({
+                    email, idToken
+                })
                 toast.success("Sign in successfully.");
                 router.push('/');
             }
@@ -56,6 +82,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
             toast.error(`There was an error: ${error}`);
         }
     }
+
     const isSignIn = type === 'sign-in';
     return (
         <div className="card-border lg:min-w-[560px]">
---
+++ firebase/client.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyACI-dL3kAjDMCBEtoQEj0rd4mTzSfa848",
+    authDomain: "interviewready-4e175.firebaseapp.com",
+    projectId: "interviewready-4e175",
+    storageBucket: "interviewready-4e175.firebasestorage.app",
+    messagingSenderId: "861174176675",
+    appId: "1:861174176675:web:062e319d59c57fb73a9816",
+    measurementId: "G-YCN9TLZ9X4"
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
